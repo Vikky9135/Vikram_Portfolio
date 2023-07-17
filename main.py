a47353f7a89bc05c4557f20ff14db8ce2b4f668b
Vikram <vikramvikky2003@gmail.com>
2023-07-18 02:15:38 +0530

    Add description

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("Images/photo.png", width=280)
+    st.image("Images/photo.png")
 with col2:
     st.title("Vikram's")
-    content = """ 
+    content1 = """ 
     Hi... I am Vikram ... Programmer, web developer and Blockchain Engineer.
     I am a student at Reva University with B.Tech degree in CSE-Artificial Engineering and Machine Learning .
     I have worked on many minor projects on web development and Blockchain Technology.  
     """
-    st.info(content)
+    st.info(content1)
+content2 = """
+Below You can find some of the apps I built in Python.
+"""
+st.write(content2)
+
