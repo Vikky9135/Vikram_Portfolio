543548a69c5bf4dcfbe0438136945a35c2e15fc6
Vikky9135 <vikramvikky2003@gmail.com>
2024-08-16 08:20:56 +0530

    Update 2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns([1.5, 1.5])
 
 with col1:
-    st.image("Images/photo.png", width=350)
+    st.image("images/photo.png", width=350)
 with col2:
     st.title("Vikram's")
     content1 = """ 
